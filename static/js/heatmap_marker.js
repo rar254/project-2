78cd01e44139046d6a3ef177cc7d959635e967eb
Rafael <stephany@Rafaels-Air.fios-router.home>
2021-01-27 15:54:50 -0500

    deleted extra py and js files

--- static/js/heatmap_marker.js
+++ static/js/heatmap_marker.js
@@ -14,23 +14,35 @@ L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
 
 var url = "https://data.sfgov.org/resource/cuks-n6tp.json?$limit=10000";
 
-d3.json(url, function(response) {
+d3.json("/json-data", function(response) {
 
   console.log(response);
 
   var heatArray = [];
 
   for (var i = 0; i < response.length; i++) {
-    var location = response[i].location;
+    var location = response[i];
 
     if (location) {
-      heatArray.push([location.coordinates[1], location.coordinates[0]]);
+      heatArray.push([location.Latitude, location.Longitude]);
     }
+  
+    L.circle(heatArray[i], {
+      fillOpacity: 0.50,
+      color: "black",
+      fillColor: "red",
+      // Adjust radius
+      radius: location.accidents * 1500
+    }).bindPopup("<h1>" + location.country+ " #" + location.number + "</h1> <hr> <h3>Accidents: " + location.accidents + "</h3>" + "<hr> <h3>Fatalities: " + location.fatalities + "</h3>").addTo(myMap);
+    
   }
 
-  var heat = L.heatLayer(heatArray, {
-    radius: 20,
-    blur: 35
-  }).addTo(myMap);
+  console.log(heatArray);
+  console.log(location.accidents);
+
+  // var heat = L.heatLayer(heatArray, {
+  //   radius: 50,
+  //   blur: 350
+  // }).addTo(myMap);
 
 });
