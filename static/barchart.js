ff10d4a6b82ac81ea3f0b54129a8d0392b19dd14
JordanChat <69655794+JordanChat@users.noreply.github.com>
2021-01-28 20:47:50 -0500

    Create barchart.js

---
+++ static/barchart.js
@@ -0,0 +1,29 @@
+d3.json("/json-data-continents", function (myData) { 
+    console.log(myData); 
+      
+    var myXData = [];
+    var myYData = [];
+  
+    for (var i = 0; i < myData.length; i++){
+      var xData = myData[i][continents];
+      var yData = myData[i].fatalities;
+      console.log(xData);
+      myXData.push(xData);
+      myYData.push(yData);
+    };
+  
+    var data = [
+        {
+          x: myXData,
+          y: myYData,
+          marker:{
+            color: myXData,
+          },
+          type: 'bar'
+        }
+      ];
+      
+    Plotly.newPlot('my-chart', data);
+  
+  }); 
+  
